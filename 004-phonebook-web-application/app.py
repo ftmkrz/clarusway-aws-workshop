995c0d60825b80d590ea38e18b9621a385504e67
Fatma <nur.fb27@gmail.com>
2020-08-07 22:46:10 -0400

    edited

--- 004-phonebook-web-application/app.py
+++ 004-phonebook-web-application/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, url_for, request
 from flaskext.mysql import MySQL
+#from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_HOST'] = 'call-mysql-db-server.cbanmzptkrzf.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("DB_HOST", 'last-test.cmo1zcgk7qya.us-east-1.rds.amazonaws.com')
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Admin123'
-app.config['MYSQL_DATABASE_DB'] = 'phone_book'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Admin12345'
+app.config['MYSQL_DATABASE_DB'] = 'phonebook'
 app.config['MYSQL_DATABASE_PORT'] = 3306
+
 
 mysql = MySQL()
 mysql.init_app(app)
@@ -16,7 +19,7 @@ connection.autocommit(True)
 cursor = connection.cursor()
 
 def init_phonebook_db():
-    drop_table = 'DROP TABLE IF EXISTS phonebook;'
+    drop_table = 'DROP TABLE IF EXISTS phonebook.phonebook;'
     phonebook_table = '''
     CREATE TABLE phonebook(
         id INTEGER NOT NULL AUTO_INCREMENT,
@@ -26,7 +29,7 @@ def init_phonebook_db():
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
     '''
     data = '''
-    INSERT INTO phonebook(name, number)
+    INSERT INTO phonebook.phonebook(name, number)
     VALUES
     ('John Doe', '(333)666-4422'),
     ('Jack Black', '(222)777-9955');
@@ -85,8 +88,6 @@ def delete(name):
 
 @app.route('/', methods = ['GET', 'POST'])
 def search():
-    if request.method == 'GET':
-        return render_template('index.html', developer_name ='Fatma', show_result=False)
     if request.method == 'POST':
         keyword = request.form['username']
         persons = find(keyword)
@@ -97,16 +98,21 @@ def search():
 def add():
     if request.method == 'POST':
         keyword = request.form['username'].title()
-        number = request.form['number']
+        number = request.form['phonenumber']
         if keyword.replace(" ", "").isalpha() and number.isdigit():
             result = add(name, number)
             return render_template('add-update.html', developer_name='Fatma', action_name='add', result=result, show_result=True)
-        elif keyword == " " and keyword.isdigit():
-            return render_template('add-update.html', developer_name='Fatma', not_valid=True, action_name='add', message="Error")
-        elif number == " " and number.isalpha():
-            return render_template('add-update.html', developer_name='Fatma', not_valid=True, action_name='add', message="Error")
+        elif keyword == " ":
+             return render_template('add-update.html', developer_name='Fatma', not_valid=True, action_name='add', message="Can not be empty")
+        elif keyword.isdigit():
+            return render_template('add-update.html', developer_name='Fatma', not_valid=True, action_name='add', message="Can not be digit")
+        elif number == " ":
+            return render_template('add-update.html', developer_name='Fatma', not_valid=True, action_name='add', message="Can not be empty")
+        elif not number.isdigit():
+            return render_template('add-update.html', developer_name='Fatma', not_valid=True, action_name='add', message="Can not be alpha")
     else:
         return render_template('add-update.html', developer_name='Fatma', action_name='add', show_result=False)
+
 @app.route('/', methods = ['GET', 'POST'])
 def delete():
     if request.method == 'POST':
@@ -121,9 +127,9 @@ def delete():
 
 if __name__ == '__main__':
     init_phonebook_db()
-    app.run('0.0.0.0', port = 80, debug = True)
+    #app.run('0.0.0.0', port = 80, debug = True)
     #app.run(debug=True)
-    #app.run('localhost', port=5000, debug=True)
+    app.run('localhost', port=5000, debug=True)
 
 
     
